19d2ea9aa915cbebcbf0855977d0604fad28c77c
Nghi-PC <nghi@gmail.com>
2023-02-23 17:06:03 +0700

    test 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My first Pipeline learn react
+          My first Pipeline 2 learn react
         </a>
       </header>
     </div>
